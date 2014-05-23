9aa231e92c589e1357285477f20f8f43d131c33a
Romain <r.esilva@sysdream.com>
2014-05-23 18:46:41 +0200

    Add first draw of the api without data collection

---
+++ index.php
@@ -0,0 +1,69 @@
+<?php
+// Autoload managed by composer.
+require 'vendor/autoload.php';
+
+// Load repositories.
+require 'lib/repositories/Users.class.php';
+require 'lib/repositories/Recipes.class.php';
+
+// Load Slim framework
+$app = new \Slim\Slim();
+
+/**
+ * Recipe related methods.
+ */
+$app->get('/recipe/:ruid', function($ruid) {
+    $recipe = Recipes::retrieveById($ruid);
+
+    if ($recipe) {
+        $response = array('status' => true, 'data' => $recipe);
+    }
+    else {
+        $response = array('status' => false);
+    }
+
+    echo json_encode($response);
+});
+
+/**
+ * User related methods.
+ */
+$app->get('/user/:uid', function($uid) {
+    $user = Users::retrieveById($uid);
+
+    if ($user) {
+        $response = array('status' => true, 'data' => $user);
+    }
+    else {
+        $response = array('status' => false);
+    }
+
+    echo json_encode($response);
+});
+$app->get('/logout', function() {
+    $response = array('status' => false, 'data' => array());
+
+    echo json_encode($response);
+});
+
+$app->delete('/recipe/:ruid', function($ruid) {
+    try {
+        Recipes::deleteById($ruid);
+
+        $response = array('status' => true, 'data' => array());
+    }
+    catch ($error) {
+        $response = array('status' => false, 'data' => $error);
+    }
+
+    echo json_encode($response);
+});
+
+/**
+ * Other related methods.
+ */
+$app->get('infos', function() {
+    $stats = new array();
+
+    echo json_encode($stats);
+});
