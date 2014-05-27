b9869316f516e7a89c1d37b649d2496bf3cfaa04
Romain <r.esilva@sysdream.com>
2014-05-28 01:19:54 +0200

    Replace recipes repository and update retrieveById() method

--- src/Vrap/TheBlenderCommunity/Repositories/Recipes.php
+++ src/Vrap/TheBlenderCommunity/Repositories/Recipes.php
@@ -1,16 +1,36 @@
 <?php
 namespace Vrap\TheBlenderCommunity\Repositories;
 
-class Recipes {
+class Recipes extends \Vrap\TheBlenderCommunity\Repository {
     public static function retrieveAll() {
 
     }
 
-    public static function retriveById($ruid) {
+    public static function retrieveById($ruid) {
+        $sql = '
+            SELECT
+                `uuid`, `name`, `author`, `created`, `updated`, `forked`
+            FROM
+                `recipes`
+            WHERE
+                `uuid` = :ruid
+        ';
 
+        $stmt = self::getDatabase()->prepare($sql);
+        $stmt->bindValue(':ruid', $ruid);
+
+        $stmt->execute();
+
+        $recipe = $stmt->fetch(\PDO::FETCH_ASSOC);
+
+        if (false === $stmt) {
+            return false;
+        }
+
+        return $recipe;
     }
 
-    public static function retriveByUser($uid) {
+    public static function retrieveByUser($uid) {
 
     }
 
