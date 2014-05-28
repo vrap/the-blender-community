67531a5ffe107e22c9a0235810f56e9a93a98f44
Romain <r.esilva@sysdream.com>
2014-05-28 19:59:19 +0200

    Implement retrieveAll method in Users repository

--- src/Vrap/TheBlenderCommunity/Repositories/Users.php
+++ src/Vrap/TheBlenderCommunity/Repositories/Users.php
@@ -1,7 +1,28 @@
 <?php
 class Users {
+    /**
+     * Retrieve all existing recipes
+     * 
+     * @return Array An array with recipes data
+     */
     public static function retrieveAll() {
+        $sql = '
+            SELECT
+                `uuid`, `username`
+            FROM
+                `users`
+        ';
 
+        $stmt = self::getDatabase()->prepare($sql);
+        $stmt->execute();
+
+        $users = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+        if (false === $stmt) {
+            return false;
+        }
+
+        return $users;
     }
 
     public static function retrieveById($id) {
