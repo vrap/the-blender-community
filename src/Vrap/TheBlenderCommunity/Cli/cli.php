f043c447b160ae5e573fd1d7fd2d94bd63bda7a4
Pierre <pflauder@gmail.com>
2014-05-31 14:34:06 +0200

    CLI command to list Resources

--- src/Vrap/TheBlenderCommunity/Cli/cli.php
+++ src/Vrap/TheBlenderCommunity/Cli/cli.php
@@ -15,33 +15,150 @@
  * Use print function to display anything to CLI
  */
 
-// FIY, behavior is largely inspired by the behavior of namp.
+// https://github.com/nategood/commando
 
 require_once 'vendor/autoload.php';
 
-print 'Community CLI - beta (https://github.com/vrap/the-blender-community)' . "\n";
+use Vrap\TheBlenderCommunity\Repositories;
+use Vrap\TheBlenderCommunity;
 
-$cliCmd = new Commando\Command();
+$configuration = TheBlenderCommunity\Configurator::getInstance()->load('config.ini');
 
-// Define first option
-$cliCmd->option()
+print 'Community CLI - beta (https://github.com/vrap/the-blender-community)' . "\n\n";
+
+$listCmd = new Commando\Command();
+
+// Define a flag "-l" a.k.a. "--list"
+$listCmd->flag('l')
     ->require()
-    ->describedAs('Resource');
+    ->aka('list')
+    ->describedAs('List all items of a resource')
+    ->must(function($arg) {
+        $resources = array('User', 'Recipe');
 
-// Define a flag "-c" a.k.a. "--read"
-$cliCmd->option('r')
-    ->aka('read')
-    ->describedAs('Read a resource')
-    ->must(function($title) {
-        $titles = array('Mister', 'Mr', 'Misses', 'Mrs', 'Miss', 'Ms');
-        
-        return in_array($title, $titles);
+        return in_array($arg, $resources);
     });
 
+// Define a flag "-n" a.k.a. "--number"
+$listCmd->flag('n')
+    ->aka('number')
+    ->describedAs('Number of rows to retrive')
+    ->must(function($arg) {
+        return (is_numeric($arg) && $arg > 0);
+    });
+
+$allResource  = array();
+$resource     = $listCmd['l'];
+$numberOfRows = (int)$listCmd['n'];
+
+switch ($resource) {
+    case 'User':
+        $allResource = Repositories\Users::retrieveAll();
+        
+        break;
+    case 'Recipe':
+        $allResource = Repositories\Recipes::retrieveAll();
+
+        break;
+    default:
+        
+        break;
+}
+
+if (! empty($allResource)) {
+    $columns = '';
+    $items   = '';
+    $counter = 0;
+
+    if (! empty($numberOfRows)) {
+        $allResource = array_slice($allResource, 0, $numberOfRows);
+    }
+
+    foreach ($allResource[0] as $column => $value) {
+        $columns .= $column . sizeColumn($column);
+    }
+
+    foreach ($allResource as $item => $field) {
+        $line = '';
+
+        foreach (array_keys($field) as $key => $value) {
+            $line .= $field[$value] . sizeColumn($field[$value]);
+        }
+
+        $items .= $line . "\n";
+
+        $counter++;
+    }
+
+    $return = 'List the ' . $counter . ' items of ' . $resource . ':' . "\n\n" .
+        $columns . "\n" .
+        $items;
+
+    echo $return;
+}
+else {
+    echo 'No ' . $resource . ' founded.' . "\n";
+
+    return;
+}
+
+function sizeColumn($column) {
+    $widthColumns  = 20;
+    $spacesCounter = ($widthColumns - strlen($column)); 
+    $spaces        = '';
+
+    for ($i = 0; $i < $spacesCounter; $i++) {
+        $spaces .=  ' ';
+    }
+
+    $spaces .= '|';
+
+    return $spaces;
+}
+
+
+
+
+// Define first option
+/*$readCmd->argument()
+    ->require()
+    ->describedAs('Resource')
+    ->must(function($resource) {
+        $resources = array('User', 'Recipe');
+
+        return in_array($resource, $resources);
+    });*/
+
+//var_dump($readCmd->getArgumentValues());
+//var_dump($readCmd->getFlagValues());
+
+
+/*
 // Define a flag "-d" aka "--delete"
-$cliCmd->option('d')
+$cliCmd->flag('d')
     ->aka('delete')
     ->describedAs('Delete a resource');
+
+// Define a flag "-l" aka "--list"
+$cliCmd->flag('l')
+    ->aka('list')
+    ->describedAs('List all items of a resource');
+
+// Define a flag "--reset-password"
+$cliCmd->flag('reset-password')
+    ->describedAs('Reset a User password');
+
+
+
+
+
+// Read a Resource:              cli.php -r Resource ResourceId
+// Delete a Resource:            cli.php -d Resource ResourceId
+// List all items of a Resource: cli.php -l Resource
+// Reset a USer password:        cli.php --reset-password UserId
+
+
+
 
 
 /*
