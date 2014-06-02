33b2a5435b36640c739b69a2bd6ffc5bc44de5e5
Romain <r.esilva@sysdream.com>
2014-06-02 23:32:27 +0200

    Improve indent of the main file

--- index.php
+++ index.php
@@ -20,150 +20,174 @@ $app->response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, 
 /**
  * Recipe related methods.
  */
-$app->get('/recipes/:ruid', function($ruid) {
-    $recipe = Repositories\Recipes::retrieveById($ruid);
+$app->get(
+    '/recipes/:ruid',
+    function($ruid) {
+        $recipe = Repositories\Recipes::retrieveById($ruid);
 
-    if ($recipe) {
-        // Retrieve steps of the recipe
-        $steps = Repositories\RecipeSteps::retrieveByRecipe($recipe);
+        if ($recipe) {
+            // Retrieve steps of the recipe
+            $steps = Repositories\RecipeSteps::retrieveByRecipe($recipe);
 
-        // If steps exists, add params of each steps
-        if ($steps) {
-            foreach ($steps as $key => $step) {
-                $params = Repositories\RecipeStepValues::retrieveByRecipeStep($step);
+            // If steps exists, add params of each steps
+            if ($steps) {
+                foreach ($steps as $key => $step) {
+                    $params = Repositories\RecipeStepValues::retrieveByRecipeStep($step);
 
-                $steps[$key]['params'] = $params;
+                    $steps[$key]['params'] = $params;
+                }
             }
+            else {
+                $steps = array();
+            }
+
+            // Add steps to the recipe and return the response
+            $recipe['steps'] = $steps;
+
+            $response = array('status' => true, 'data' => $recipe);
         }
         else {
-            $steps = array();
+            $response = array('status' => false);
         }
 
-        // Add steps to the recipe and return the response
-        $recipe['steps'] = $steps;
-
-        $response = array('status' => true, 'data' => $recipe);
+        echo json_encode($response);
     }
-    else {
-        $response = array('status' => false);
+);
+
+$app->get(
+    '/recipes',
+    function() {
+        $recipes = Repositories\Recipes::retrieveAll();
+
+        if ($recipes) {
+            $response = array('status' => true, 'data' => $recipes);
+        }
+        else {
+            $response = array('status' => false);
+        }
+
+        echo json_encode($response);
     }
+);
 
-    echo json_encode($response);
-});
+$app->delete(
+    '/recipes/:ruid',
+    function($ruid) {
+        if (Repositories\Recipes::remove($ruid)) {
+            $response = array('status' => true, 'data' => array());
+        }
+        else {
+            $response = array('status' => false);
+        }
 
-$app->get('/recipes', function() {
-    $recipes = Repositories\Recipes::retrieveAll();
-
-    if ($recipes) {
-        $response = array('status' => true, 'data' => $recipes);
+        echo json_encode($response);
     }
-    else {
-        $response = array('status' => false);
-    }
-
-    echo json_encode($response);
-});
-
-$app->delete('/recipes/:ruid', function($ruid) {
-    if (Repositories\Recipes::remove($ruid)) {
-        $response = array('status' => true, 'data' => array());
-    }
-    else {
-        $response = array('status' => false);
-    }
-
-    echo json_encode($response);
-});
+);
 
 /**
  * User related methods.
  */
-$app->get('/users/:uid', function($uid) {
-    $user = Repositories\Users::retrieveById($uid);
+$app->get(
+    '/users/:uid',
+    function($uid) {
+        $user = Repositories\Users::retrieveById($uid);
 
-    if ($user) {
-        $response = array('status' => true, 'data' => $user);
+        if ($user) {
+            $response = array('status' => true, 'data' => $user);
+        }
+        else {
+            $response = array('status' => false);
+        }
+
+        echo json_encode($response);
     }
-    else {
-        $response = array('status' => false);
+);
+
+$app->get(
+    '/users',
+    function() {
+        $users = Repositories\Users::retrieveAll();
+
+        if ($users) {
+            $response = array('status' => true, 'data' => $users);
+        }
+        else {
+            $response = array('status' => false);
+        }
+
+        echo json_encode($response);
     }
+);
 
-    echo json_encode($response);
-});
+$app->get(
+    '/register',
+    function() use($app) {
+        $config = \Vrap\TheBlenderCommunity\Configurator::getInstance();
+        $params = $config->get('params', false);
+        $response = array('status' => false, 'data' => array());
 
-$app->get('/users', function() {
-    $users = Repositories\Users::retrieveAll();
+        if ($params && isset($params['private']) && (bool)$params['private'] === true) {
+            $response['data']['error'] = 'ERR_PRIVATE_COMMUNITY';
+        }
+        else {
+            $username = $app->request()->post('username');
+            $email    = $app->request()->post('email');
+            $password = $app->request()->post('password');
 
-    if ($users) {
-        $response = array('status' => true, 'data' => $users);
-    }
-    else {
-        $response = array('status' => false);
-    }
+            if (!empty($username) && !empty($email) && !empty($password)) {
+                // Validate email
+                $pattern = '/^[a-zA-Z0-9+&*-]+(?:\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,7}$/';
+                $validateEmail = preg_match($pattern, $email);
 
-    echo json_encode($response);
-});
+                if (!$validateEmail) {
+                    $response['data']['error'] = 'ERR_EMAIL_NOT_VALID';
+                }
+                elseif (Repositories\Users::retrieveByEmail($email)) {
+                    $response['data']['error'] = 'ERR_EMAIL_ALREADY_EXIST';
+                }
+                elseif (Repositories\Users::retrieveByUsername($username)) {
+                    $response['data']['error'] = 'ERR_USERNAME_ALREADY_EXIST';
+                }
 
-$app->get('/register', function() use($app) {
-    $config = \Vrap\TheBlenderCommunity\Configurator::getInstance();
-    $params = $config->get('params', false);
-    $response = array('status' => false, 'data' => array());
-
-    if ($params && isset($params['private']) && (bool)$params['private'] === true) {
-        $response['data']['error'] = 'ERR_PRIVATE_COMMUNITY';
-    }
-    else {
-        $username = $app->request()->post('username');
-        $email    = $app->request()->post('email');
-        $password = $app->request()->post('password');
-
-        if (!empty($username) && !empty($email) && !empty($password)) {
-            // Validate email
-            $pattern = '/^[a-zA-Z0-9+&*-]+(?:\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,7}$/';
-            $validateEmail = preg_match($pattern, $email);
-
-            if (!$validateEmail) {
-                $response['data']['error'] = 'ERR_EMAIL_NOT_VALID';
-            }
-            elseif (Repositories\Users::retrieveByEmail($email)) {
-                $response['data']['error'] = 'ERR_EMAIL_ALREADY_EXIST';
-            }
-            elseif (Repositories\Users::retrieveByUsername($username)) {
-                $response['data']['error'] = 'ERR_USERNAME_ALREADY_EXIST';
-            }
-
-            if (!isset($response['data']['error'])) {
-                if (Repositories\Users::save($username, $email, $password)) {
-                    $response['status'] = true;
+                if (!isset($response['data']['error'])) {
+                    if (Repositories\Users::save($username, $email, $password)) {
+                        $response['status'] = true;
+                    }
                 }
             }
         }
+
+        echo json_encode($response);
     }
+);
 
-    echo json_encode($response);
-});
+$app->get(
+    '/logout',
+    function() {
+        $response = array('status' => false, 'data' => array());
 
-$app->get('/logout', function() {
-    $response = array('status' => false, 'data' => array());
-
-    echo json_encode($response);
-});
+        echo json_encode($response);
+    }
+);
 
 /**
  * Other related methods.
  */
-$app->get('/infos', function() {
-    $infos = array();
-    $config = \Vrap\TheBlenderCommunity\Configurator::getInstance();
+$app->get(
+    '/infos',
+    function() {
+        $infos = array();
+        $config = \Vrap\TheBlenderCommunity\Configurator::getInstance();
 
-    $infos['general'] = $config->get('community');
-    $infos['stats'] = array(
-        'users' => Repositories\Users::count(),
-        'recipes' => Repositories\Recipes::count()
-    );
+        $infos['general'] = $config->get('community');
+        $infos['stats'] = array(
+            'users' => Repositories\Users::count(),
+            'recipes' => Repositories\Recipes::count()
+        );
 
-    echo json_encode($infos);
-});
+        echo json_encode($infos);
+    }
+);
 
 /**
  * Start Slim application.
