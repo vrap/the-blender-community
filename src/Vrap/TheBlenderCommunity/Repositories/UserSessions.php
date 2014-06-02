125122777eecb5097b1a142fa680b33a02ec296f
Romain <r.esilva@sysdream.com>
2014-06-03 00:31:13 +0200

    Add a retrieveByToken method to UserSession repository

--- src/Vrap/TheBlenderCommunity/Repositories/UserSessions.php
+++ src/Vrap/TheBlenderCommunity/Repositories/UserSessions.php
@@ -2,4 +2,27 @@
 namespace Vrap\TheBlenderCommunity\Repositories;
 
 class UserSessions extends \Vrap\TheBlenderCommunity\Repository {
+    public static function retrieveByToken($token) {
+        $sql = '
+            SELECT
+                `user`, `token`
+            FROM
+                `user_sessions`
+            WHERE
+                `token` = :token
+        ';
+
+        $stmt = self::getDatabase()->prepare($sql);
+        $stmt->bindValue(':token', $token);
+
+        $stmt->execute();
+
+        $userSession = $stmt->fetch(\PDO::FETCH_ASSOC);
+
+        if (false === $stmt) {
+            return false;
+        }
+
+        return $userSession;
+    }
 }
