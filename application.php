414ac8404c444cd5e048b863ca1206cf1c41421b
FPierre <pflauder@gmail.com>
2014-06-26 10:53:33 +0200

    Correction

--- application.php
+++ application.php
@@ -6,6 +6,7 @@ require 'vendor/autoload.php';
 
 use Vrap\TheBlenderCommunity\Cli\ListCommand;
 use Vrap\TheBlenderCommunity\Cli\ReadCommand;
+use Vrap\TheBlenderCommunity\Cli\DeleteCommand;
 use \Symfony\Component\Console\Application;
 
 $application = new Application('Community', 'beta');
